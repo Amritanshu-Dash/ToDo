9e12deb88377312b47cfbe3bee76446d1fa3406c
amritanshu_dash <amritanshudash6677@gmail.com>
2023-08-24 18:35:19 +0530

    final commit of 24aug with realm

--- ToDo/Controllers/ToDoListViewController.swift
+++ ToDo/Controllers/ToDoListViewController.swift
@@ -117,11 +117,7 @@ extension ToDoListViewController: UISearchBarDelegate{
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-//        let request: NSFetchRequest<Item> = Item.fetchRequest()
-//        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-        loadItems()
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "title", ascending: true)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
